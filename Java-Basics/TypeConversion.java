5e1af2b0132fb630d272ccc019d9af085b6c2e6e
Annapurna <annapurna.shibu@gmail.com>
2026-02-21 21:51:01 +0530

    Converted Java-Basics to normal folder and added programs

---
+++ Java-Basics/TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        double result = (double) 500 / 1000;
+        System.out.println("Result: " + result);
+
+        double value = 20 + 45.68;
+        System.out.println("Value: " + value);
+    }
+}
---
+++ Java-Basics/VariableNaming.java
@@ -0,0 +1,9 @@
+public class VariableNaming {
+    public static void main(String[] args) {
+        int salary = 100000;
+        int annualSalary = 1000000;
+
+        System.out.println("Salary: " + salary);
+        System.out.println("Annual Salary: " + annualSalary);
+    }
+}
