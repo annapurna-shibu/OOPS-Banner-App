1d8b6093b5c81d140184d65752de0be78243550c
Annapurna <annapurna.shibu@gmail.com>
2026-03-02 13:14:24 +0530

    Add OOPSBannerUC5

---
+++ OOPSBannerUC5.java
@@ -0,0 +1,21 @@
+public class OOPSBannerUC5 {
+
+    public static void main(String[] args) {
+
+        // UC5: Inline array initialization using String.join()
+        String[] lines = {
+            String.join(" ", "*****", " *****", " *****", " *****"),
+            String.join(" ", "*   *", " *   *", " *   *", " *   *"),
+            String.join(" ", "*   *", " *   *", " *   *", " *   *"),
+            String.join(" ", "*****", " *****", " *****", " *****"),
+            String.join(" ", "*     ", " *     ", " *     ", " *     "),
+            String.join(" ", "*     ", " *     ", " *     ", " *     "),
+            String.join(" ", "*****", " *****", " *****", " *****")
+        };
+
+        // Enhanced for loop to print banner
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
